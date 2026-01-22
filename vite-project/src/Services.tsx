1e37404426c649dd73c878e4f06c597efdbe6448
talk223 <t.alkattan04@gmail.com>
2026-01-22 15:34:33 -0500

    rehehe

--- vite-project/src/Services.tsx
+++ vite-project/src/Services.tsx
@@ -1,7 +1,7 @@
-import Header from "./Header.tsx";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+import Header from "./Header.tsx"; 
+import List from "@mui/material/List"; 
+import ListItem from "@mui/material/ListItem"; 
+import ListItemText from "@mui/material/ListItemText"; 
 import ListSubheader from "@mui/material/ListSubheader";
 
 export default function Services() {
@@ -15,24 +15,50 @@ export default function Services() {
   return (
     <div>
       <Header />
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "background.paper",
-          position: "relative",
-          overflow: "auto",
-          maxHeight: 300,
-          "& ul": { padding: 0 },
+
+      {/* Centering wrapper */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "60px",
         }}
-        subheader={<ListSubheader>Our Services</ListSubheader>}
       >
-        {services.map((service, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={service} />
-          </ListItem>
-        ))}
-      </List>
+        <List
+          sx={{
+            width: "600px",              // make it bigger
+            bgcolor: "#111",             // match your theme
+            color: "white",
+            borderRadius: "12px",
+            padding: "24px",
+          }}
+          subheader={
+            <ListSubheader
+              sx={{
+                bgcolor: "#111",
+                color: "white",
+                fontSize: "28px",        // bigger title
+                textAlign: "center",
+                marginBottom: "16px",
+              }}
+            >
+              Our Services
+            </ListSubheader>
+          }
+        >
+          {services.map((service, index) => (
+            <ListItem key={index} sx={{ justifyContent: "center" }}>
+              <ListItemText
+                primary={service}
+                primaryTypographyProps={{
+                  fontSize: "22px",     // bigger items
+                  textAlign: "center",
+                }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 }
